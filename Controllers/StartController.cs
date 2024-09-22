006e7bde06388e13d20f7e6ea14a9c296fa569e9
kbardach <144119408+kbardach@users.noreply.github.com>
2024-09-22 12:37:38 +0200

    Adding bunch of stuffs

---
+++ Controllers/StartController.cs
@@ -0,0 +1,33 @@
+﻿using kim_umbraco.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace kim_umbraco.Controllers
+{
+	public class StartController : RenderController
+	{
+		private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+		public StartController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+		{
+			_umbracoContextAccessor = umbracoContextAccessor;
+		}
+
+		public override IActionResult Index()
+		{
+			var startPage = CurrentPage as Start;
+
+			if (startPage != null)
+			{
+				var model = new StartPageViewModel(startPage, _umbracoContextAccessor);
+
+				return View("Start", model);
+			}
+
+			return null;
+		}
+	}
+}
---
+++ Models/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace kim_umbraco.Models.ViewModels
+{
+    public class StartPageViewModel : BasePageModel<Start>
+    {
+        public StartPageViewModel(Start content, IUmbracoContextAccessor umbracoContextAccessor) : base(content, umbracoContextAccessor)
+        {
+        }
+    }
+}
